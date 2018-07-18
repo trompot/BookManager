5c6d533529d1ecbf368fe7fbd7b45005e3b59ba3
DESKTOP-824R233\tromp <i9714@stdmail.nara-k.ac.jp>
2018-07-19 01:13:33 +0900

    javascriptでごりごり

--- js/main.js
+++ js/main.js
@@ -1,25 +1,78 @@
 // ファイル読み込み
-$(function (handleload) {
+// $(function (handleload) {
+//     var req = new XMLHttpRequest();
+
+//     req.addEventListener("load", handleload, false);
+//     req.open("GET", "csv/dat_book.csv", true);
+//     req.send(null);     // httpリクエストの発行
+
+//     req.onload = function() {
+//         convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+//     };
+// });
+
+// openbdで本のデータを取得
+function getBookData(isbn){
+    // const url = 'https://api.openbd.jp/v1/get?isbn=' + isbn;
     var req = new XMLHttpRequest();
+    req.onreadystatechange = function()
+    {
+        if( this.readyState == 4 && this.status == 200 )
+        {
+            if( this.response )
+            {
+                console.log(this.response);
+                // 読み込んだ後処理したい内容をかく
 
-    req.addEventListener("load", handleload, false);
-    req.open("GET", "csv/dat_book.csv", true);
-    req.send(null);     // httpリクエストの発行
+            }
+        }
+    }
 
-    req.onload = function() {
-        convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-    };
+    req.open( 'GET', 'https://api.openbd.jp/v1/get?isbn='+isbn, true );
+    req.responseType = 'json';
+    req.send( null );
+}
+
+
+// main
+$(function(handleload){
+    // readFile("csv/dat_book.csv", handleload);
+    getBookData("9784043878024");
 });
 
+// ファイルの読み込み
+function readFile(file, handleload){
+    var req = new XMLHttpRequest();
+    req.addEventListener("load", handleload, false);
+    req.open("GET", file, true);
+    req.send(null);
+    req.onload = function(){
+        convertCSVtoArray(req.responseText);
+    };
+}
 
 // CSVをいい感じに変換
 function convertCSVtoArray(str) {
     var result = [];
     var tmp = str.split("\n");
     console.log(tmp.length);
-    for (var i = 1; i < tmp.length; ++i) {
+    for (var i = 1; i < tmp.length; i++) {
         result[i] = tmp[i].split('\t');
     }
+    for(var j = 0; j< tmp.length; j++){
+        for(var i = 2; i< tmp.length; i++){
+            if(result[i][1] < result[i-1][1]){
+                var temp = result[i-1];
+                result[i-1] = result[i];
+                result[i] = temp;
+                getBookData(result[5]);
+            }
+        }
+    }
+
+    
+    
+    // result[1].sort();
     initVue(result);
 }
 
